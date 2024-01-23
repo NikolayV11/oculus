5444c9907a8789037e49b59acc7e42a570a1f388
Николай <wasilev-n@mail.ru>
2024-01-23 16:22:12 +0500

    main top

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { HandySvg } from "handy-svg";
 
 import logo from "./img/Oculus_Quest_logo.svg";
 import basket from "./img/basket.svg";
+import { url } from "inspector";
 
 function App() {
   return (
@@ -39,6 +40,22 @@ function App() {
           </nav>
         </div>
       </header>
+      <main className="main">
+        <div className="top">
+          <div className="container">
+            <h1 className="title">Meta Quest 2</h1>
+            <p className="subtitle">Get an Elite Strap free</p>
+            <p className="top__text">
+              For a limited time only, save $59.99 USD. Offer ends 1/19/23
+            </p>
+            <button className="buy-btn">
+              <span className="buy-btn__text">BUY NOW</span>
+              <span className="buy-btn__price">399,99 USD</span>
+            </button>
+            <p className="buy__text">Meta Quest 2 is for ages 13+</p>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
