aabe61c4be52ea97d98b4c0cd8d5a08862336eca
Николай <wasilev-n@mail.ru>
2024-01-23 22:59:13 +0500

    tabs section

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,71 @@ import { HandySvg } from "handy-svg";
 
 import logo from "./img/Oculus_Quest_logo.svg";
 import basket from "./img/basket.svg";
-import { url } from "inspector";
+import play from "./img/play.svg";
 
+type tabsBtnItemType = { title: string; data: number };
+type tabsContentType = { title: string; text: string; urlImg: string; textBtn: string };
+type listBtnType = {
+  item: tabsBtnItemType;
+  index: number;
+};
+
+const tabsBtnItem: tabsBtnItemType[] = [
+  { title: "01 Graphics", data: 0 },
+  { title: "02 Controls", data: 1 },
+  { title: "03 Processor", data: 2 },
+  { title: "04 Set up your play area", data: 3 },
+  { title: "05 Gardian activity", data: 4 },
+  { title: "06 Headset casting", data: 5 },
+];
+
+const tabsContent: tabsContentType[] = [
+  {
+    title: "Graphics",
+    text: "With 1832 x 1920 pixels per eye, everything from multiplayer games and productivity apps to 360 0 videos look beyond incredible",
+    urlImg: "/img/content_1.jpg",
+    textBtn: "Watch the video",
+  },
+  {
+    title: "Graphics",
+    text: "With 1832 x 1920 pixels per eye, everything from multiplayer games and productivity apps to 360 0 videos look beyond incredible",
+    urlImg: "/img/content_1.jpg",
+    textBtn: "Watch the video",
+  },
+  {
+    title: "Graphics",
+    text: "With 1832 x 1920 pixels per eye, everything from multiplayer games and productivity apps to 360 0 videos look beyond incredible",
+    urlImg: "/img/content_1.jpg",
+    textBtn: "Watch the video",
+  },
+  {
+    title: "Graphics",
+    text: "With 1832 x 1920 pixels per eye, everything from multiplayer games and productivity apps to 360 0 videos look beyond incredible",
+    urlImg: "/img/content_1.jpg",
+    textBtn: "Watch the video",
+  },
+  {
+    title: "Graphics",
+    text: "With 1832 x 1920 pixels per eye, everything from multiplayer games and productivity apps to 360 0 videos look beyond incredible",
+    urlImg: "/img/content_1.jpg",
+    textBtn: "Watch the video",
+  },
+
+  {
+    title: "Graphics",
+    text: "With 1832 x 1920 pixels per eye, everything from multiplayer games and productivity apps to 360 0 videos look beyond incredible",
+    urlImg: "/img/content_1.jpg",
+    textBtn: "Watch the video",
+  },
+];
 function App() {
+  const [activeBtn, setActiveBtn] = React.useState<number>(0);
+
+  function onClickBtn(dataId: number) {
+    setActiveBtn(dataId);
+  }
   return (
-    <>
+    <div>
       <header className="header">
         <div className="container">
           <nav className="nav">
@@ -41,7 +101,7 @@ function App() {
         </div>
       </header>
       <main className="main">
-        <div className="top">
+        <section className="top">
           <div className="container">
             <h1 className="title">Meta Quest 2</h1>
             <p className="subtitle">Get an Elite Strap free</p>
@@ -54,9 +114,56 @@ function App() {
             </button>
             <p className="buy__text">Meta Quest 2 is for ages 13+</p>
           </div>
-        </div>
+        </section>
+        <section className="features">
+          <div className="container">
+            <div className="features__titles">
+              <h2 className="features__title section-title">PRODUCT FEATURES</h2>
+              <p className="features__text">Easy to set up and safe to use</p>
+            </div>
+            <div className="tabs">
+              <div className="tabs__btn">
+                {tabsBtnItem.map((items): any => {
+                  return (
+                    <button
+                      key={items.data}
+                      onClick={() => onClickBtn(items.data)}
+                      className={`tabs__btn-item ${
+                        activeBtn === items.data ? "tabs__btn-item--active" : ""
+                      }`}>
+                      {items.title}
+                    </button>
+                  );
+                })}
+              </div>
+              <div className="tabs__content">
+                {tabsContent.map((item, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className={`tabs__content-item ${
+                        activeBtn === index ? "tabs__content-item--active" : ""
+                      }`}>
+                      <img className="tabs__content-img" src={item.urlImg} alt="content_1" />
+                      <div className="tabs__content-box">
+                        <h3>{item.title}</h3>
+                        <p>{item.text}</p>
+                      </div>
+                      <div className="video">
+                        <button className="video__btn">
+                          <HandySvg src={play} />
+                        </button>
+                        <span>{item.textBtn}</span>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
-    </>
+    </div>
   );
 }
 
