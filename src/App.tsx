d3d32bdab6e4ca61b4b3eb52a3683ba10c149d66
Николай <wasilev-n@mail.ru>
2024-01-25 21:36:02 +0500

    Email block

--- src/App.tsx
+++ src/App.tsx
@@ -321,6 +321,31 @@ function App() {
             </div>
           </div>
         </section>
+        <section className="contacts">
+          <div className="container">
+            <h2 className="contacts__title">Get news and updates</h2>
+            <div className="contats__inner">
+              <div className="contacts__form">
+                <form className="form">
+                  <input type="text" placeholder="E-mail" className="form__input" />
+                  <button className="form__btn" type="submit">
+                    GO
+                  </button>
+                </form>
+                <p className="contacts__text">
+                  By signing up you agree to receive updates and marketing messages (e.g. email,
+                  social, etc.) from Meta about Meta’s existing and future products and services.
+                  You may withdraw your consent and unsubscribe at any time by clicking the
+                  unsubscribe link included in our messages. Your subscription is subject to the
+                  Terms and Privacy Policy.
+                </p>
+              </div>
+              <a href="mailto:oculusshop@gmail.com" className="contacts__emil">
+                oculusshop@gmail.com
+              </a>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
